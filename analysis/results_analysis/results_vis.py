d9c3319ea5359b85a12ddb24aa34b18eca2b2d69
Antoine Dieu <dieu.antoine92@gmail.com>
2025-07-15 19:15:19 +0200

    new prompts

--- analysis/results_analysis/results_vis.py
+++ analysis/results_analysis/results_vis.py
@@ -5,15 +5,26 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from config import DATA_DIR
 
-df = pd.read_csv(DATA_DIR / "results/results_hyp_3.csv")
+# df = pd.read_csv(DATA_DIR / "results/results_hyp_3.csv")
+
+# colonnes_affichees = [
+#     "model_id",
+#     "request",
+#     "reference",
+#     "response",
+#     "reformulation",
+#     "score"
+# ]
+
+df = pd.read_csv("tests/results.csv")
 
 colonnes_affichees = [
-    "model_id",
-    "request",
-    "reference",
-    "response",
-    "reformulation",
-    "score"
+    "Request",
+    "Reference",
+    "Context",
+    "Hypothesis",
+    "Filtered Hypothesis",
+    "Response"
 ]
 
 colonnes_existantes = [col for col in colonnes_affichees if col in df.columns]
