74a2bc5dcaed19da12bc7d190842141f5cdb4400
dieuantoine <dieu.antoine92@gmail.com>
2025-08-07 16:14:58 +0200

    req in french

--- src/analysis/results_analysis/main.py
+++ src/analysis/results_analysis/main.py
@@ -13,19 +13,26 @@ if __name__ == "__main__":
         "bertscore_precision_ctx": "BERTPrecision (ctx)",
         "hyp_len": "Hypothesis Length"
     }, inplace=True)
+    
+    models = df["model_id"].unique()
 
     score_cols = ["Method 1", "Method 2 (correction)", "Method 2 (filtering)", "Method 2 (global)"]
     bertscore_cols = ["BERTRecall (ref)", "BERTPrecision (ctx)"]
     cols = score_cols + bertscore_cols + ["Hypothesis Length"]
     
-    results_path = RESULTS_ANALYSIS_DIR / "scores_analysis.csv"
-    scores_mean_tab(df, score_cols+bertscore_cols, results_path)
+    # results_path = RESULTS_ANALYSIS_DIR / "scores_analysis.csv"
+    # scores_mean_tab(df, score_cols+bertscore_cols, results_path)
 
     # bertscore_boxplot_path = RESULTS_ANALYSIS_DIR / "bertscore_boxplot.png"
     # boxplot(df, bertscore_cols, bertscore_boxplot_path)
 
     # score_boxplot_path = RESULTS_ANALYSIS_DIR / "aiter_boxplot.png"
     # boxplot(df, score_cols, score_boxplot_path)
+    
+    for model in models:
+        model_df = df[df["model_id"] == model]
+        results_path = RESULTS_ANALYSIS_DIR / f"correlation_matrix_{model}.png"
+        correlation_matrix(model_df, cols, results_path)
     
     # corr_matrix_path = RESULTS_ANALYSIS_DIR / "correlation_matrix.png"
     # correlation_matrix(df, cols, corr_matrix_path)
