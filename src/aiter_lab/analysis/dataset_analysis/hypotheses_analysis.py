afc076929c4601f1cc4a0be9151a3e34e0fc2a55
Antoine Dieu <dieu.antoine92@gmail.com>
2025-10-20 17:06:46 +0200

    data

--- src/aiter_lab/analysis/dataset_analysis/hypotheses_analysis.py
+++ src/aiter_lab/analysis/dataset_analysis/hypotheses_analysis.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from ...utils import load_from_hf
+from ...config import HYP_DS, REFERENCES_CSV
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def hyp_len_stats_tab(input_df):
-    #len_mean_tab(input_df).to_csv(output_csv, index=False)
+    input_df = input_df[input_df['hypothesis'].str.len() > 0].copy()
     input_df['hyp_length'] = input_df['hypothesis'].apply(lambda x: len(x.split()))
     hyp_df = input_df.groupby("model_id")["hyp_length"].describe().sort_values("mean")
     hyp_df = hyp_df.reset_index()
@@ -21,13 +22,13 @@ def ref_len_stats_tab(input_df):
     stats_df = pd.concat([ref_row, ctx_row])
     return stats_df
 
-# def merge_stats_tab(output_csv):
-#     hyp_df = hyp_len_stats_tab(load_from_hf(MKQA_HYP_DS))
-#     ref_df = ref_len_stats_tab(pd.read_csv(REFERENCES_CSV))
-#     cols = ['source', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
-#     merged_df = pd.concat([hyp_df[cols], ref_df[cols]])
-#     merged_df.to_csv(output_csv, index=False)
-#     return
+def merge_stats_tab(output_csv):
+    hyp_df = hyp_len_stats_tab(load_from_hf(HYP_DS))
+    ref_df = ref_len_stats_tab(pd.read_csv(REFERENCES_CSV))
+    cols = ['source', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
+    merged_df = pd.concat([hyp_df[cols], ref_df[cols]])
+    merged_df.to_csv(output_csv, index=False)
+    return
 
 def len_correlations(input_df, output_path):
     input_df['ref_length'] = input_df['reference'].apply(lambda x: len(x.split()))
--- src/aiter_lab/analysis/dataset_analysis/main.py
+++ src/aiter_lab/analysis/dataset_analysis/main.py
@@ -1,11 +1,9 @@
 import pandas as pd
 from .counters import words_counter, list_features_counter
 from .visualization import plot_msg_length, create_pie, compare_msg_length, compare_hyp_length, create_wc
-from .hypotheses_analysis import len_correlations
+from .hypotheses_analysis import len_correlations, merge_stats_tab
 from ...config import HF_TOKEN, DS_ANALYSIS_DIR, RESULTS_DIR, HYP_DS, REQ_DS, REFERENCES_CSV
 from ...utils import load_from_hf
 
 if __name__ == '__main__':
-    df = load_from_hf(REQ_DS)
-    count = words_counter(df, 'request')
-    create_wc(count, DS_ANALYSIS_DIR / 'wordcloud_requests_mkqa.png')
+    merge_stats_tab(f"{DS_ANALYSIS_DIR}/hyp_length_statistics.csv")
